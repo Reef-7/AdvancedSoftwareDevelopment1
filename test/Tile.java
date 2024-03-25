a091ae2e4b77fc7bda3fe1b8c2b71cf31ba43550
Reef-7 <Rifstudy7@gmail.com>
2024-03-25 18:05:01 +0200

    3rd commit

--- test/Tile.java
+++ test/Tile.java
@@ -1,7 +1,29 @@
 package test;
 
-
 public class Tile {
 
-	
+    public final char letter;
+    public final int score;
+
+    private Tile(char letter, int score) {
+
+        this.letter = letter;
+        this.score = score;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    public class Bag {
+
+    }
 }
