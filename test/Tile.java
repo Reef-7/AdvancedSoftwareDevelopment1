3a2acbb8784550d5bdc66403d3e0dc2ab95a5f10
Reef-7 <Rifstudy7@gmail.com>
2024-04-01 18:36:17 +0300

    updates

--- test/Tile.java
+++ test/Tile.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.Random;
+
 public class Tile {
 
     public final char letter;
@@ -15,39 +17,41 @@ public class Tile {
     public int hashCode() {
         // TODO Auto-generated method stub
         return super.hashCode();
+        // specify it according to the class
+        // implements of hashcode
     }
 
     @Override
     public boolean equals(Object obj) {
         // TODO Auto-generated method stub
         return super.equals(obj);
+        // specify it according to the class
     }
 
     public static class Bag {
 
-        int[] quantities;
-        Tile[] tiles;
-        private static Bag single_instance = null;
+        private final int[] quantities;
+        private final Tile[] tiles;
+        private static Bag singleInstance = null;
 
         private Bag() {
 
-            this.quantities = new int[26];
-            this.tiles = new Tile[26];
-
-            int[] amounts = { 9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1 };
+            this.quantities = new int[] { 9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2,
+                    1 };
+            this.tiles = new Tile[quantities.length];
             int[] values = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
-
-            for (int i = 0; i < 26; i++) {
-                quantities[i] = amounts[i];
-                // tiles[i].letter = new Tile((char) ('a' + i));
+            for (int i = 0; i < tiles.length; i++) {
                 tiles[i] = new Tile((char) ('A' + i), values[i]);
-
             }
         }
 
         public Tile getRand() {
-            double x = Math.floor(Math.random() * 26);
-            int i = (int) x;
+            // if the Bag is empty return null
+            // else for each time the quantity is zero calculate other random number and
+            // check
+            Random rnd = new Random();
+            int i = rnd.nextInt(this.quantities.length);
+
             if (quantities[i] == 0) {
                 return null;
             }
@@ -57,7 +61,7 @@ public class Tile {
         }
 
         public Tile getTile(char letter) {
-            int i = (int) (letter) - 65;
+            int i = (int) (letter) - 'A';
             if (quantities[i] == 0) {
                 return null;
             }
@@ -67,30 +71,31 @@ public class Tile {
         }
 
         public void put(Tile tile) {
-            int i = (int) (tile.letter) - 65;
+            int i = (int) (tile.letter) - 'A';
             quantities[i]++;
         }
 
         public int size() {
-            int amounts_sum = 0;
-            for (int i = 0; i < 26; i++) {
-                amounts_sum += quantities[i];
+            // another option: maintain a variable size in the class Bag
+            int amountsSum = 0;
+            for (int i = 0; i < quantities.length; i++) {
+                amountsSum += quantities[i];
             }
-            return amounts_sum;
+            return amountsSum;
         }
 
         public int[] getQuantities() {
-            int[] quantities_copy = (int[]) (quantities.clone());
-            return quantities_copy;
+            int[] quantitiesCopy = (int[]) (quantities.clone());
+            return quantitiesCopy;
 
         }
 
         public static Bag getBag() {
-            if (single_instance == null) {
+            if (singleInstance == null) {
 
-                single_instance = new Bag();
+                singleInstance = new Bag();
             }
-            return single_instance;
+            return singleInstance;
 
         }
 
