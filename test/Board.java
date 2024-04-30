6c407c4b52649021319b0bb2d3198cbab9b8ed0e
Reef-7 <Rifstudy7@gmail.com>
2024-04-30 10:17:16 +0300

    updated First Milestone

--- test/Board.java
+++ test/Board.java
@@ -1,115 +1,489 @@
 package test;
 
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
-public class Board {
-
-    private static final int BOARD_SIZE = 15;
-    private static Board singleInstance = null;
-    private final Tile[][] tiles;
-
-    enum Bonuses {
-        DoubleWord,
-        DoubleLetter,
-        TripleLetter,
-        TripleWord,
-        Star
-    }
-
-    private static final Dictionary<String, Bonuses> bonusSquares = Map.of(
-            "0,3", DoubleLetter,
-            "0,11", DoubleLetter,
-            "2,6", DoubleLetter,
-            "2,8", DoubleLetter,
-            "3,0", DoubleLetter,
-            "3,7", DoubleLetter,
-            "3,14", DoubleLetter,
-            "6,2", DoubleLetter,
-            "6,6", DoubleLetter,
-            "6,8", DoubleLetter,
-            "6,12", DoubleLetter,
-            "7,3", DoubleLetter,
-            "7,11", DoubleLetter,
-            "8,2", DoubleLetter,
-            "8,6", DoubleLetter,
-            "8,8", DoubleLetter,
-            "8,12", DoubleWord,
-            "11,0", DoubleLetter,
-            "11,7", DoubleLetter,
-            "11,14", DoubleLetter,
-            "12,6", DoubleLetter,
-            "12,8", DoubleLetter,
-            "14,3", DoubleLetter,
-            "14,11", DoubleLetter,
-            "1,5", TripleLetter,
-            "1,9", TripleLetter,
-            "5,1", TripleLetter,
-            "5,5", TripleLetter,
-            "5,9", TripleLetter,
-            "5,13", TripleLetter,
-            "9,1", TripleLetter,
-            "9,5", TripleLetter,
-            "9,9", TripleLetter,
-            "9,13", TripleLetter,
-            "13,5", TripleLetter,
-            "13,9", TripleLetter,
-            "1,1", DoubleWord,
-            "2,2", DoubleWord,
-            "3,3", DoubleWord,
-            "4,4", DoubleWord,
-            "10,10", DoubleWord,
-            "11,11", DoubleWord,
-            "12,12", DoubleWord,
-            "13,13", DoubleWord,
-            "1,13", DoubleWord,
-            "2,12", DoubleWord,
-            "3,11", DoubleLetter,
-            "4,10", DoubleLetter,
-            "10,4", DoubleLetter,
-            "11,3", DoubleWord,
-            "12,2", DoubleWord,
-            "13,1", DoubleWord,
-            "7,7", Star,
-            "0,0", TripleWord,
-            "0,7", TripleWord,
-            "0,14", TripleWord,
-            "7,0", TripleWord,
-            "7,14", TripleWord,
-            "14,0", TripleWord,
-            "14,7", TripleWord,
-            "14,14", TripleWord
-
-    );
+public class Board implements Serializable {
+    private static Board single_instance = null;
+    private final static int BOARD_HEIGHT = 15;
+    private final static int BOARD_WIDTH = 15;
+    private final Tile[][] board = new Tile[BOARD_HEIGHT][BOARD_WIDTH];
+    private static final HashMap<String, String> boardScores = new HashMap<>();
+    private final ArrayList<Word> foundWords = new ArrayList<>();
 
     private Board() {
-        this.tiles = new Tile[BOARD_SIZE][BOARD_SIZE];
-
+        initBoardScores();
     }
 
     public static Board getBoard() {
-        if (singleInstance == null) {
-            singleInstance = new Board();
+        if (single_instance == null)
+            single_instance = new Board();
+        return single_instance;
+    }
+
+    private void initBoardScores() {
+        // STAR BLOCK
+        boardScores.put("0707", "2W");
+
+        // TRIPLE WORD
+        boardScores.put("0000", "3W");
+        boardScores.put("0700", "3W");
+        boardScores.put("0007", "3W");
+        boardScores.put("0014", "3W");
+        boardScores.put("1400", "3W");
+        boardScores.put("1407", "3W");
+        boardScores.put("0714", "3W");
+        boardScores.put("1414", "3W");
+
+        // DOUBLE WORD
+        boardScores.put("0101", "2W");
+        boardScores.put("0202", "2W");
+        boardScores.put("0303", "2W");
+        boardScores.put("0404", "2W");
+        boardScores.put("0113", "2W");
+        boardScores.put("0212", "2W");
+        boardScores.put("0311", "2W");
+        boardScores.put("0410", "2W");
+        boardScores.put("1301", "2W");
+        boardScores.put("1202", "2W");
+        boardScores.put("1103", "2W");
+        boardScores.put("1004", "2W");
+        boardScores.put("1010", "2W");
+        boardScores.put("1111", "2W");
+        boardScores.put("1212", "2W");
+        boardScores.put("1313", "2W");
+
+        // TRIPLE LETTER
+        boardScores.put("0501", "3L");
+        boardScores.put("0901", "3L");
+        boardScores.put("0105", "3L");
+        boardScores.put("0505", "3L");
+        boardScores.put("0905", "3L");
+        boardScores.put("1305", "3L");
+        boardScores.put("0109", "3L");
+        boardScores.put("0509", "3L");
+        boardScores.put("0909", "3L");
+        boardScores.put("1309", "3L");
+        boardScores.put("0513", "3L");
+        boardScores.put("0913", "3L");
+
+        // DOUBLE LETTER
+        boardScores.put("0300", "2L");
+        boardScores.put("1100", "2L");
+        boardScores.put("0602", "2L");
+        boardScores.put("0802", "2L");
+        boardScores.put("0003", "2L");
+        boardScores.put("0703", "2L");
+        boardScores.put("1403", "2L");
+        boardScores.put("0206", "2L");
+        boardScores.put("0606", "2L");
+        boardScores.put("0806", "2L");
+        boardScores.put("1206", "2L");
+        boardScores.put("0307", "2L");
+        boardScores.put("1107", "2L");
+        boardScores.put("0208", "2L");
+        boardScores.put("0608", "2L");
+        boardScores.put("0808", "2L");
+        boardScores.put("1208", "2L");
+        boardScores.put("0011", "2L");
+        boardScores.put("0711", "2L");
+        boardScores.put("1411", "2L");
+        boardScores.put("0612", "2L");
+        boardScores.put("0812", "2L");
+        boardScores.put("0314", "2L");
+        boardScores.put("1114", "2L");
+    }
+
+    private int getScore(Word word) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        int score = 0;
+        int wordMultiplier = 1;
+        int tileMultiplier = 1;
+
+        for (int i = 0; i < wordLength; i++) {
+            String boardRef;
+            if (word.getVertical()) {
+                boardRef = getBoardRefString(row + i, col);
+            } else {
+                boardRef = getBoardRefString(row, col + i);
+            }
+            if (Objects.equals(boardScores.get(boardRef), "2W")) {
+                wordMultiplier = wordMultiplier * 2;
+            }
+            if (Objects.equals(boardScores.get(boardRef), "3W")) {
+                wordMultiplier = wordMultiplier * 3;
+            }
+            if (Objects.equals(boardScores.get(boardRef), "2L")) {
+                tileMultiplier = 2;
+            }
+            if (Objects.equals(boardScores.get(boardRef), "3L")) {
+                tileMultiplier = 3;
+            }
+            score += word.getTiles()[i].score * tileMultiplier;
+            if (boardRef.equals("0707")) {
+                boardScores.remove("0707");
+            }
+            tileMultiplier = 1;
+
         }
-        return singleInstance;
+        score = score * wordMultiplier;
+        return score;
+    }
+
+    private String getBoardRefString(int r, int c) {
+        String row, col;
+        if (r < 10)
+            row = String.format("%02d", r);
+        else
+            row = String.valueOf(r);
+        if (c < 10)
+            col = String.format("%02d", c);
+        else
+            col = String.valueOf(c);
+
+        return row + col;
     }
 
     public Tile[][] getTiles() {
-
-        // use array.copy
-        Tile[][] copy = new Tile[BOARD_SIZE][BOARD_SIZE];
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                copy[i][j] = tiles[i][j];
-            }
+        Tile[][] boardCopy = new Tile[BOARD_HEIGHT][BOARD_WIDTH];
+        for (int i = 0; i < board.length; i++) {
+            System.arraycopy(this.board[i], 0, boardCopy[i], 0, this.board[0].length);
         }
-        return copy;
+        return boardCopy;
     }
 
-    private boolean dictionaryLegal(Word word) {
-        // Implementation: check if the word is legal according to the given dictionary
+    public boolean boardLegal(Word word) {
+        if (isBoardEmpty() && isWordOnStarSquare(word) && wordIsInBounds(word))
+            return true;
+        return wordIsInBounds(word) && wordHasAdjacentOrOverlappingTile(word) && noChangeOfTilesNeeded(word);
+    }
+
+    private boolean isBoardEmpty() {
+        for (int i = 0; i < BOARD_WIDTH; i++) {
+            for (int j = 0; j < BOARD_HEIGHT; j++) {
+                if (board[i][j] != null)
+                    return false;
+            }
+        }
         return true;
     }
 
+    private boolean isWordOnStarSquare(Word word) {
+        int wordLength = word.getTiles().length;
+        if (word.getVertical()) {
+            if (word.getCol() == 7)
+                return word.getRow() + wordLength > 7;
+        } else {
+            if (word.getRow() == 7)
+                return word.getCol() + wordLength > 7;
+        }
+        return false;
+    }
+
+    private boolean wordIsInBounds(Word word) {
+        if (word.allTilesAreNulls())
+            return false;
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        if (row < 0 || row >= BOARD_HEIGHT || col < 0 || col >= BOARD_WIDTH)
+            return false;
+
+        if (word.getVertical())
+            return word.getRow() + wordLength <= BOARD_HEIGHT;
+        else
+            return word.getCol() + wordLength <= BOARD_WIDTH;
+    }
+
+    private boolean wordHasAdjacentOrOverlappingTile(Word word) {
+        if (word.getVertical())
+            return checkAdjacencyForVertical(word);
+        else
+            return checkAdjacencyForHorizontal(word);
+    }
+
+    private boolean checkAdjacencyForVertical(Word word) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        if (row - 1 >= 0 && board[row - 1][col] != null)
+            return true;
+
+        if (row + wordLength + 1 < BOARD_HEIGHT && board[row + wordLength + 1][col] != null)
+            return true;
+
+        for (int i = 0; i < wordLength; i++) {
+
+            if (col >= 1 && col <= 13) {
+                if (board[row + i][col - 1] != null || board[row + i][col + 1] != null)
+                    return true;
+            }
+
+            if (col == BOARD_WIDTH - 1) {
+                if (board[row + i][col - 1] != null)
+                    return true;
+            }
+
+            if (col == 0) {
+                if (board[row + i][col + 1] != null)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean checkAdjacencyForHorizontal(Word word) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        if (col - 1 >= 0 && board[row][col - 1] != null)
+            return true;
+
+        if (col + wordLength + 1 < BOARD_WIDTH && board[row][col + wordLength + 1] != null)
+            return true;
+
+        for (int i = 0; i < wordLength; i++) {
+
+            if (row >= 1 && row <= 13) {
+                if (board[row - 1][col + i] != null || board[row + 1][col + i] != null)
+                    return true;
+            }
+
+            if (row == BOARD_HEIGHT - 1) {
+                if (board[row - 1][col + i] != null)
+                    return true;
+            }
+
+            if (row == 0) {
+                if (board[row + 1][col + i] != null)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean noChangeOfTilesNeeded(Word word) {
+        Tile[][] boardCopy = getTiles();
+        addWordToBoard(word, boardCopy);
+
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+        int countOfSameLetters = 0;
+
+        for (int i = 0; i < wordLength; i++) {
+            if (word.getVertical()) {
+                if (word.getTiles()[i] == null || board[row + i][col] == null)
+                    continue;
+                if (boardCopy[row + i][col] == board[row + i][col]) {
+                    countOfSameLetters++;
+                    continue;
+                }
+            } else {
+                if (word.getTiles()[i] == null || board[row][col + i] == null)
+                    continue;
+                if (boardCopy[row][col + i] == board[row][col + i]) {
+                    countOfSameLetters++;
+                    continue;
+                }
+            }
+            return false;
+        }
+        return wordLength != countOfSameLetters;
+    }
+
+    public boolean dictionaryLegal(Word word) {
+        return true;
+    }
+
+    private ArrayList<Word> getWords(Word word) {
+        ArrayList<Word> newWords = new ArrayList<>();
+
+        if (isBoardEmpty()) {
+
+            if (!isWordComplete(word)) {
+                return newWords;
+            }
+            newWords.add(word);
+            this.foundWords.add(word);
+            return newWords;
+        }
+
+        Word completeWord = findCompleteWord(word);
+        if (completeWord.hasNullTiles()) {
+            return newWords;
+        }
+        newWords.add(completeWord);
+
+        findNewWords(word, newWords);
+        this.foundWords.addAll(newWords);
+
+        return newWords;
+    }
+
+    private boolean isWordComplete(Word word) {
+        int wordLength = word.getTiles().length;
+        for (int i = 0; i < wordLength; i++) {
+            if (word.getTiles()[i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    private void findNewWords(Word word, ArrayList<Word> words) {
+        if (word.getVertical())
+            findHorizontalNewWords(word, words);
+
+        if (!word.getVertical())
+            findVerticalNewWords(word, words);
+    }
+
+    private void findHorizontalNewWords(Word word, ArrayList<Word> words) {
+        Tile[][] boardCopy = getTiles();
+        addWordToBoard(word, boardCopy);
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+        int j, length, wordStart;
+
+        for (int i = 0; i < wordLength; i++) {
+            wordStart = col;
+            j = 1;
+            length = 1;
+            while (col - j >= 0 && boardCopy[row + i][col - j] != null) {
+                wordStart = col - j;
+                j++;
+                length++;
+            }
+            j = 1;
+            while (col + j < BOARD_WIDTH && boardCopy[row + i][col + j] != null) {
+                j++;
+                length++;
+            }
+            if (length > 1) {
+                Tile[] tiles = new Tile[length];
+                System.arraycopy(boardCopy[row + i], wordStart, tiles, 0, length);
+                Word newWord = new Word(tiles, row + i, wordStart, false);
+                if (!wordWasAlreadyFound(newWord)) {
+                    words.add(newWord);
+                }
+            }
+        }
+    }
+
+    private void findVerticalNewWords(Word word, ArrayList<Word> words) {
+        Tile[][] boardCopy = getTiles();
+        addWordToBoard(word, boardCopy);
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+        int j, length, wordStart;
+
+        for (int i = 0; i < wordLength; i++) {
+            wordStart = row;
+            j = 1;
+            length = 1;
+            while (row - j >= 0 && boardCopy[row - j][col + i] != null) {
+                wordStart = row - j;
+                j++;
+                length++;
+            }
+            j = 1;
+            while (row + j < BOARD_HEIGHT && boardCopy[row + j][col + i] != null) {
+                j++;
+                length++;
+            }
+            if (length > 1) {
+                Tile[] tiles = new Tile[length];
+                for (int k = 0; k < length; k++) {
+                    tiles[k] = boardCopy[wordStart + k][col + i];
+                }
+                Word newWord = new Word(tiles, wordStart, col + i, true);
+                if (!wordWasAlreadyFound(newWord)) {
+                    words.add(newWord);
+                }
+            }
+        }
+    }
+
+    private boolean wordWasAlreadyFound(Word word) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        for (Word foundWord : foundWords) {
+            if (foundWord.getRow() == row && foundWord.getCol() == col && foundWord.getTiles().length == wordLength) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Word findCompleteWord(Word word) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+        Tile[] tiles = new Tile[wordLength];
+        if (word.getVertical()) {
+            for (int i = 0; i < wordLength; i++) {
+                if (word.getTiles()[i] == null)
+                    tiles[i] = board[row + i][col];
+                else
+                    tiles[i] = word.getTiles()[i];
+            }
+        } else {
+            for (int i = 0; i < wordLength; i++) {
+                if (word.getTiles()[i] == null)
+                    tiles[i] = board[row][col + i];
+                else
+                    tiles[i] = word.getTiles()[i];
+            }
+        }
+        word.setTiles(tiles);
+        return word;
+    }
+
+    public int tryPlaceWord(Word newWord) {
+        if (!boardLegal(newWord))
+            return -1;
+
+        ArrayList<Word> createdWords = getWords(newWord);
+
+        for (Word word : createdWords)
+            if (!dictionaryLegal(word))
+                return 0;
+        addWordToBoard(newWord, this.board);
+        int score = 0;
+        for (Word word : createdWords) {
+            score += getScore(word);
+        }
+
+        return score;
+    }
+
+    private void addWordToBoard(Word word, Tile[][] board) {
+        int row = word.getRow();
+        int col = word.getCol();
+        int wordLength = word.getTiles().length;
+
+        if (word.getVertical()) {
+            for (int i = 0; i < wordLength; i++) {
+                board[row + i][col] = word.getTiles()[i];
+            }
+        } else {
+            for (int i = 0; i < wordLength; i++) {
+                board[row][col + i] = word.getTiles()[i];
+            }
+        }
+    }
 }
--- test/Word.java
+++ test/Word.java
@@ -1,48 +1,77 @@
 package test;
 
-public class Word {
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
 
-    public final Tile[] tiles;
-    public final int row;
-    public final int col;
-    public final boolean vertical;
+public class Word implements Serializable {
+    private Tile[] tiles;
+    private final int row;
+    private final int col;
+    private final boolean vertical;
 
     public Word(Tile[] tiles, int row, int col, boolean vertical) {
-
-        this.tiles = tiles;
+        Tile[] temp = new Tile[tiles.length];
+        System.arraycopy(tiles, 0, temp, 0, tiles.length);
+        this.tiles = temp;
         this.row = row;
         this.col = col;
         this.vertical = vertical;
     }
 
-    public Tile[] getTiles() {
+    public void setTiles(Tile[] tiles) {
+        Tile[] temp = new Tile[tiles.length];
+        System.arraycopy(tiles, 0, temp, 0, tiles.length);
+        this.tiles = temp;
+    }
 
-        return this.tiles;
+    public Tile[] getTiles() {
+        return tiles;
     }
 
     public int getRow() {
-        return this.row;
+        return row;
     }
 
     public int getCol() {
-        return this.col;
+        return col;
     }
 
     public boolean getVertical() {
-        return this.vertical;
+        return vertical;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object o) {
+        if (this == o)
             return true;
-        }
-        if (obj == null || this.getClass() != obj.getClass()) {
+        if (o == null || getClass() != o.getClass())
             return false;
+        Word word = (Word) o;
+        return row == word.row && col == word.col && vertical == word.vertical && Arrays.equals(tiles, word.tiles);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(row, col, vertical);
+        result = 31 * result + Arrays.hashCode(tiles);
+        return result;
+    }
+
+    public boolean hasNullTiles() {
+        for (Tile tile : tiles) {
+            if (tile == null) {
+                return true;
+            }
         }
-        Word other = (Word) obj;
-        if (tiles != other.tiles || row != other.row || col != other.col || vertical != other.vertical) {
-            return false;
+        return false;
+    }
+
+    public boolean allTilesAreNulls() {
+        int wordLength = getTiles().length;
+        for (int i = 0; i < wordLength; i++) {
+            if (getTiles()[i] != null)
+                return false;
         }
         return true;
     }
